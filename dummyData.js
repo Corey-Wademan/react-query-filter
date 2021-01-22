2dd90efcfba26778f93b32056316fb3634c18711
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-01-22 17:08:43 -0500

    Create dummyData.js

---
+++ dummyData.js
@@ -0,0 +1,21 @@
+const dummyData = [
+  {
+    title: "a hard interview question",
+    tags: ["sql", "probability", "modelling"],
+    companies: ["facebook", "linkedin", "netflix"],
+    positions: ["software engineer", "Business Analyst", "Data Scientist"]
+  },
+  {
+    title: "easy question to solve",
+    tags: ["sql", "probability", "statistics"],
+    companies: ["facebook", "linkedin", "amazon", "google"],
+    positions: [
+      "software engineer",
+      "marketing Analyst",
+      "Data Scientist",
+      "ML Engineer"
+    ]
+  }
+];
+
+export default dummyData;
